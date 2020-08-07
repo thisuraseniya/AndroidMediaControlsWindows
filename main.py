abb71353702faa63b00f2237f818bd59db936f3b
Thisura Seniya Rathnayake <seniyaratnayake@gmail.com>
2020-08-07 07:26:19 +0530

    Cleanup

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+from threading import Event
 import sounddevice as sd
 import win32api
-from threading import Event
-
 
 SAMPLE_RATE = 10000  # Sample rate for our input stream
 BLOCK_SIZE = 100  # Number of samples before we trigger a processing callback
 PRESS_SECONDS = 0.1  # Number of seconds button should be held to register press
 PRESS_SAMPLE_THRESHOLD = 0.7  # Signal amplitude to register as a button press
 VK_MEDIA_PLAY_PAUSE = 0xB3
-BLOCKS_TO_PRESS = (SAMPLE_RATE/BLOCK_SIZE)*PRESS_SECONDS
+BLOCKS_TO_PRESS = (SAMPLE_RATE / BLOCK_SIZE) * PRESS_SECONDS
 
 
 class HeadsetButtonController:
     def process_frames(self, indata, frames, time, status):
-        mean = sum([y for x in indata[:] for y in x])/len(indata[:])
+        mean = sum([y for x in indata[:] for y in x]) / len(indata[:])
 
         if mean < PRESS_SAMPLE_THRESHOLD:
             self.times_pressed += 1
